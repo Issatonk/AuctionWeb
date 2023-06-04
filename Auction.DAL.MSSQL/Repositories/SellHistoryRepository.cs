6f7775b1c92108c6ac53182dbb18bb6120e9eb1e
Issatonk <77848472+Issatonk@users.noreply.github.com>
2023-06-04 19:34:22 +0300

    SellHistoryRepository: Created create and getMany methods

--- Auction.DAL.MSSQL/Repositories/SellHistoryRepository.cs
+++ Auction.DAL.MSSQL/Repositories/SellHistoryRepository.cs
@@ -1,15 +1,55 @@
 ﻿using Auction.DAL.MSSQL.Entity;
 using Auction.Interfaces.DAL;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Query;
+using System.Linq;
 using System.Linq.Expressions;
 
 namespace Auction.DAL.MSSQL.Repositories;
 
 public class SellHistoryRepository : IRepository<SellHistory>
 {
-    public Task<SellHistory> Create(SellHistory entity)
+    private readonly DbSet<SellHistory> _sellHistoryDbSet;
+
+    public SellHistoryRepository(AuctionContext context)
     {
-        throw new NotImplementedException();
+        _sellHistoryDbSet = context.SellHistories;
+    }
+    public async Task<SellHistory> Create(SellHistory entity)
+    {
+        return (await _sellHistoryDbSet.AddAsync(entity)).Entity;
+    }
+    public async Task<IEnumerable<SellHistory>> GetMany(
+        Expression<Func<SellHistory, bool>>? filtres = null, 
+        Func<IQueryable<SellHistory>, IOrderedQueryable<SellHistory>>? sorts = null, 
+        Func<IQueryable<SellHistory>, IIncludableQueryable<SellHistory, object>>? include = null, 
+        int pageIndex = 0, 
+        int pageSize = 20, 
+        bool disableTracking = true)
+    {
+        IQueryable<SellHistory> query = _sellHistoryDbSet;
+
+        if (disableTracking)
+        {
+            query = query.AsNoTracking();
+        }
+        if (include is not null)
+        {
+            query = include(query);
+        }
+        if (filtres is not null)
+        {
+            query = query.Where(filtres);
+        }
+        if (sorts is not null)
+        {
+            query = sorts(query);
+        }
+
+        return await query
+            .Skip(pageIndex * pageSize)
+            .Take(pageSize)
+            .ToListAsync();
     }
 
     public Task<SellHistory> Delete(SellHistory entity)
@@ -18,11 +58,6 @@ public class SellHistoryRepository : IRepository<SellHistory>
     }
 
     public Task<SellHistory?> GetFirstOrDefault(Expression<Func<SellHistory, bool>> filter, Func<IQueryable<SellHistory>, IOrderedQueryable<SellHistory>>? sorts = null, Func<IQueryable<SellHistory>, IIncludableQueryable<SellHistory, object>>? include = null, bool disableTracking = false)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<IEnumerable<SellHistory>> GetMany(Expression<Func<SellHistory, bool>>? filtres = null, Func<IQueryable<SellHistory>, IOrderedQueryable<SellHistory>>? sorts = null, Func<IQueryable<SellHistory>, IIncludableQueryable<SellHistory, object>>? include = null, int pageIndex = 0, int pageSize = 20, bool disableTracking = false)
     {
         throw new NotImplementedException();
     }
