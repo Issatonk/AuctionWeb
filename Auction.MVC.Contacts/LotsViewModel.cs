0bfda7f28eed087bc58a7d160a412fbb9f4bca3f
Issatonk <77848472+Issatonk@users.noreply.github.com>
2023-06-07 19:36:25 +0300

    Created LotsViewModel, Changed model: IEnumerable<Lot> -> LotsViewModel

---
+++ Auction.MVC.Contacts/LotsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Auction.DAL.MSSQL.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auction.MVC.Contracts
+{
+    public class LotsViewModel
+    {
+        public IEnumerable<Lot> Lots { get; set; }
+    }
+}
