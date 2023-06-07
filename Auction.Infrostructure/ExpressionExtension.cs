8486c1d71d3dd8dc5f13683db4c26ebfd37d60fe
Issatonk <77848472+Issatonk@users.noreply.github.com>
2023-06-07 19:39:58 +0300

    Added expression method 'Add'

---
+++ Auction.Infrostructure/ExpressionExtension.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace Auction.Infrostructure;
+
+public static class ExpressionExtension
+{
+    public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> left, Expression<Func<T, bool>> right)
+    {
+        var parameter = Expression.Parameter(typeof(T));
+        var body = Expression.AndAlso(Expression.Invoke(left, parameter), Expression.Invoke(right, parameter));
+        return Expression.Lambda<Func<T, bool>>(body, parameter);
+    }
+
+}
