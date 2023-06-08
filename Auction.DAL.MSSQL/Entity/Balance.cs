909a153a4de02cd2a8e9d4016d58204b388eb834
Issatonk <77848472+Issatonk@users.noreply.github.com>
2023-06-08 15:59:19 +0300

    Now the user class is inherited from IdentityUser, the balance is placed in a separate class

---
+++ Auction.DAL.MSSQL/Entity/Balance.cs
@@ -0,0 +1,8 @@
+﻿namespace Auction.DAL.MSSQL.Entity;
+
+public class Balance
+{
+    public decimal Amount { get; set; }
+
+    public string CurrencyCode { get; set; } = "RUB";
+}
--- Auction.DAL.MSSQL/Entity/User.cs
+++ Auction.DAL.MSSQL/Entity/User.cs
@@ -1,30 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
 
 namespace Auction.DAL.MSSQL.Entity;
 
-/// <summary>
-/// Users personal account
-/// </summary>
-public class User
+public class User : IdentityUser<Guid>
 {
-    [Key]
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    public Guid Id { get; set; }
-
     [Required]
-    [MaxLength(50)]
-    public string Name { get; set; }
-
-    [Required]
-    [MaxLength(50)]
-    public string Password { get; set; }
-
-    [Required]
-    public double Balance { get; set; }
+    public Balance Balance { get; set; }
 
 }
